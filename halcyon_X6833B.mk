ba39669b017a989c234edfda23881afa23f093b4
Apichat1234inwza007 <discom610@gmail.com>
2026-05-17 14:19:03 +0700

    X6833B: halycon Bringup
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- halcyon_X6833B.mk
+++ halcyon_X6833B.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/infinix/X6833B/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 BOARD_VENDOR := Infinix
-PRODUCT_NAME := lineage_X6833B
+PRODUCT_NAME := halcyon_X6833B
 PRODUCT_DEVICE := X6833B
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -26,7 +26,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=X6833B \
     BuildFingerprint=Infinix/X6833B-GL/Infinix-X6833B:14/UP1A.231005.007/240703V808:user/release-keys
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
 
